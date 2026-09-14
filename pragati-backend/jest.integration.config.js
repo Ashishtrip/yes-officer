5e48409393150c98f8244a31e3e64373782e7d43
somyagupta1122 <gupta.somya1412@gmail.com>
2026-09-15 01:48:19 +0530

    somya commit #2

--- pragati-backend/jest.integration.config.js
+++ pragati-backend/jest.integration.config.js
@@ -3,6 +3,7 @@ const config = require('./jest.config');
 
 module.exports = {
   ...config,
+  roots: ['<rootDir>'],
   testMatch: ['**/?(*.)+(integration.test).ts'],
   testPathIgnorePatterns: ['/node_modules/', '/dist/'],
 };
